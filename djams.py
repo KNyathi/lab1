d05616bc3ff745540ba328a0735b9f3923301753
badradine <bodourali235@gmail.com>
2023-09-20 14:20:54 +0300

    je adjoute les codes

---
+++ djams.py
@@ -0,0 +1,8 @@
+ int rlt1 = gcd(48, 18);
+        int rlt2 = gcd(52, 8);
+        int rlt3 = gcd(259, 28);
+
+        System.out.println("gcd(48, 18) = " + rlt1);
+        System.out.println("gcd(52, 8) = " + rlt2);
+        System.out.println("gcd(259, 28) = " + rlt3);
+        
